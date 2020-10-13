761d07879226c7cda5c8f57c512d86a9b95b80c6
Marc-Antoine Ferland <maferland@hey.com>
2020-10-12 22:48:10 -0400

    :sparkles: Add example project

---
+++ example/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {GithubUserActivity} from '@maferland/github-activity'
+
+function App() {
+  return <GithubUserActivity />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
